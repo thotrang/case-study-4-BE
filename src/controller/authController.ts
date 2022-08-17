c41041c3bfc57b449e9999c8198243b43981ecbc
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-08-18 00:16:36 +0700

    fix register and role

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,40 +1,99 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 import { User } from "../model/user";
-import {SECRET_KEY} from "../middleware/auth"
+import { SECRET_KEY } from "../middleware/auth";
+import { Role } from "../model/role";
 class AuthController {
     register = async (req: Request, res: Response) => {
-        let registerForm = req.body;
-        registerForm.passwourd = await bcrypt.hash(registerForm.passwourd,10)
-        let user = await User.create(registerForm);
+        let user = req.body;
+        if (user.password != user.rePassword) {
+            res.status(500).json({
+                message: 'Re-password fales'
+            })
+        } else {
+            user.password = await bcrypt.hash(user.password, 10);
+            user.role = ["62fd161bfd34f0abdd2051d0"];
+            user.status = 1;
+            user = await User.create(user);
+            await Role.updateMany({ '_id': user.role }, { $push: { users: user._id } });
+            res.status(201).json(user);
+        }
+    }
+    register1 = async (req, res) => {
+        let user = req.body;
+        user.password = await bcrypt.hash(user.password, 10);
+        user = await User.create(user);
         res.status(201).json(user);
     }
-    login = async (req: Request,res: Response) => {
+    login = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            let loginForm = req.body;
+            let user = await User.findOne(loginForm);
+            if (!user) {
+                res.status(401).json({
+                    message: 'Username is not existed!'
+                })
+            } else {
+                if (user.status != 1) {
+                    res.status(401).json({
+                        message: 'Your account is locked'
+                    })
+                } else {
+                    let comparePassword = await bcrypt.compare(loginForm.passwourd, user.password);
+                    if (!comparePassword) {
+                        res.status(401).json({
+                            message: "Password is fales"
+                        })
+                    } else {
+                        let payload = {
+                            username: user.username
+                        }
+                        let token = await jwt.sign(payload, SECRET_KEY, {
+                            expiresIn: 360000
+                        })
+                        res.status(201).json({
+                            token: token
+                        })
+                    }
+                }
+
+            }
+        } catch (err) {
+            next(err)
+        }
+
+    }
+
+
+    login1 = async (req, res) => {
         let loginForm = req.body;
-        let user = await User.findOne(loginForm);
-        if(!user){
+        let user = await User.findOne({
+            username: loginForm.username
+        });
+        if (!user) {
             res.status(401).json({
                 message: 'Username is not existed!'
             })
-        }else{
-            let comparePassword = await bcrypt.compare(loginForm.passwourd,user.password);
-            if(!comparePassword){
+        } else {
+            let comparePassword = await bcrypt.compare(loginForm.password, user.password);
+            if (!comparePassword) {
                 res.status(401).json({
-                    message:"Password is fales"
+                    message: 'Password is wrong'
                 })
-            }else{
+            } else {
                 let payload = {
-                    username:user.username
+                    username: user.username
                 }
-                let token = await jwt.sign(payload,SECRET_KEY,{
-                    expiresIn: 360000
-                })
-                res.status(201).json({
-                    token:token
-                })
+                let token = await jwt.sign(payload, SECRET_KEY, {
+                    expiresIn: 36000
+                });
+                res.status(200).json({
+                    token: token
+                });
             }
         }
     }
+
 }
 export default new AuthController();
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,4 +1,8 @@
 import { Router } from "express";
 import { routerAuth } from "./auth.router";
+import { routerUser } from "./user.router";
+
 export const router = Router();
 router.use('',routerAuth);
+router.use('/admin/:id',routerUser);
+router.use('/user/:id',routerUser);
