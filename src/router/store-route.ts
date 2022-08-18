be6cc42a52b83a0ed82d81147ff281a914f032b1
ha.nguyen1003 <hanguyen18052004@gmail.com>
2022-08-18 16:34:24 +0700

    store model

---
+++ src/router/store-route.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import storeController from "../controller/storeController";
+
+
+
+export const storeRoute = Router();
+storeRoute.get('', storeController.getAll);
+storeRoute.post('', storeController.addStore);
+storeRoute.delete('/:id', storeController.deleteStore);
+storeRoute.get('/search?',storeController.searchStore);
+storeRoute.get('/detail/:id',storeController.getStore);
+storeRoute.put('/:id',storeController.updateStore);
