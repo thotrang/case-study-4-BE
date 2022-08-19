a571b673530439f5ee197b4a63f50f15caf763ee
ntai02 <sanjiokama2002@gmail.com>
2022-08-19 08:14:52 +0700

    item

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,22 +1,22 @@
 import { model, Schema } from "mongoose"
 import {IRole} from "./role";
 interface IUser{
-    // name: string,
-    // avatar: string,
-    // username:string,
-    // password: string,
-    // role: IRole
+    name: string,
+    avatar: string,
+    username:string,
+    password: string,
+    role: IRole
 
 }
 const UserSchema = new Schema<IUser>({
-    // name:String,
-    // avatar:String,
-    // username:String,
-    // password:String,
-    // role: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Role'
-    // }
+    name:String,
+    avatar:String,
+    username:String,
+    password:String,
+    role: {
+        type: Schema.Types.ObjectId,
+        ref: 'Role'
+    }
 
 })
 export const User = model<IUser>('User',UserSchema);
