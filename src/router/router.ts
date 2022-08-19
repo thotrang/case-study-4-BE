86d098d5bd8b8673073ca2fe1859b16d60d8f58e
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-08-19 09:57:22 +0700

    fit authorization

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { routerAuth } from "./auth.router";
 import { routerUser } from "./user.router";
-import { checkAdmin } from "../middleware/auth";
 
 export const router = Router();
 router.use('',routerAuth);
-router.use('/admin/:id',checkAdmin, routerUser);
+router.use('/admin/:id', routerUser);
 router.use('/user/:id',routerUser);
